032bcb31bec21518b21eea9b733cb2d9a505d5b1
grek-andrian <gretsko.andrian@gmail.com>
2017-01-18 00:37:02 +0200

    Convert interface to a class

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+var Task = (function () {
+    function Task(priority, title) {
+        this.priority = priority;
+        this.title = title;
+    }
+    Task.prototype.toString = function () {
+        return "Priority: " + this.priority + ", Title: " + this.title;
+    };
+    return Task;
+}());
 var tasks = [];
 document.getElementById('btn-add').addEventListener('click', function (e) {
     e.preventDefault();
-    var priority = document.getElementById('task-priority')['value'];
-    var title = document.getElementById('task-title')['value'];
-    tasks.push({
-        priority: priority,
-        title: title
-    });
+    var task = new Task(Number(document.getElementById('task-priority')['value']), String(document.getElementById('task-title')['value']));
+    tasks.push(task);
     var domString = '';
     tasks.forEach(function (t) {
-        domString += "<li>Priority: " + t.priority + ", Title: " + t.title + ", Completed: " + t.completed + "</li>";
+        domString += "<li>" + t.toString() + "</li>";
     });
     document.getElementById('task-list').innerHTML = domString;
 });
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
-interface Task {
-    priority: number,
-    title: string
+class Task {
+    private priority: number;
+    private title: string;
+    constructor(priority: number, title: string) {
+        this.priority = priority;
+        this.title = title;
+    }
+    toString(): string {
+        return `Priority: ${this.priority}, Title: ${this.title}`;
+    }
 }
 
-let tasks = [];
+let tasks: Task[] = [];
 document.getElementById('btn-add').addEventListener('click', e => {
     e.preventDefault();
-    const priority = document.getElementById('task-priority')['value'];
-    const title = document.getElementById('task-title')['value'];
-    tasks.push({
-        priority: priority,
-        title: title
-    });
+    const task = new Task(Number(document.getElementById('task-priority')['value']),
+                          String(document.getElementById('task-title')['value']));
+    tasks.push(task);
     let domString = '';
     tasks.forEach(t => {
-        domString += `<li>Priority: ${t.priority}, Title: ${t.title}, Completed: ${t.completed}</li>`;
+        domString += `<li>${t.toString()}</li>`;
     });
     document.getElementById('task-list').innerHTML = domString;
 });
